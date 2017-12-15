26153bd7bc1934e60058bcf7a4ae2310da510fb6
Dave Fravel <dave@bluegear.io>
2017-12-15 13:15:52 -0500

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,38 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :countdown,
+  ecto_repos: [Countdown.Repo]
+
+# Configures the endpoint
+config :countdown, CountdownWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "CCW5QhdXNPn+h+WpDJDJTU0RuB8vDeK0m22KP5oJ6pvBgfQIh4jrHsuMyD4kAtsV",
+  render_errors: [view: CountdownWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Countdown.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+#  we're using Auth0 as part of this tutorial. I've never used it before
+#  and have doubts about it since I'm used to building my own logins
+
+config :auth0_ex,
+  domain: System.get_env("AUTH0_DOMAIN"),
+  mgmt_client_id: System.get_env("AUTH0_MGMT_CLIENT_ID"),
+  mgmt_client_secret: System.get_env("AUTH0_MGMT_CLIENT_SECRET"),
+  http_opts: []
+
+  
---
+++ lib/countdown_web/views/layout_view.ex
@@ -0,0 +1,8 @@
+defmodule CountdownWeb.LayoutView do
+  use CountdownWeb, :view
+
+
+  def cookies(conn, cookie_name) do
+    conn.cookies[cookie_name]
+  end
+end
